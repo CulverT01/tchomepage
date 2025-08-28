7c820b3c20bef3e7592bded76c2ddaf562f2df2a
CulverT01 <tculverwell01@gmail.com>
2025-08-28 03:30:46 +0100

    update nvmrc

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Toby Culverwell's Blog",
 	url: "https://main--toby-culverwell-01.netlify.app/",
 	language: "en",
-	description: "I am ehibiting my web design and development skills",
+	description: "I am exhibiting my web design and development skills",
 	author: {
 		name: "Toby Culverwell",
 		email: "tculverwell@gmail.com",
